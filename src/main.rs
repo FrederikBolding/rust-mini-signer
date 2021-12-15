70c39985734a552358e3e16c24fdf8d9093ce6ba
Frederik Bolding <frederik.bolding@gmail.com>
2021-12-15 21:55:06 +0100

    Initial EIP 1559 implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use primitive_types::U256;
+use std::str::FromStr;
+mod signer;
+
+use signer::{Signer, Type2Transaction};
+
 fn main() {
-    println!("Hello, world!");
+    // Test private key - not worth anything :)
+    let private_key = "eaf2c50dfd10524651e7e459c1286f0c2404eb0f34ffd2a1eb14373db49fceb6";
+    let signer = Signer::new(private_key.to_string());
+    let tx = Type2Transaction {
+        chain_id: U256::from_str("0x03").unwrap(),
+        nonce: U256::from_str("0x06").unwrap(),
+        max_priority_fee_per_gas: U256::from_str("0x3b9aca00").unwrap(),
+        max_fee_per_gas: U256::from_str("0x4a817c800").unwrap(),
+        gas_limit: U256::from_str("0x5208").unwrap(),
+        to: U256::from_str("0xB2BB2b958aFA2e96dAb3F3Ce7162B87dAea39017").unwrap(),
+        value: U256::from_str("0x2386f26fc10000").unwrap(),
+        data: U256::from_str("0x").unwrap(),
+        access_list: U256::from_str("0x").unwrap(),
+    };
+    let sig = signer.sign_type2_tx(tx);
+    println!("{}", sig);
+    assert_eq!(sig, "02f8720306843b9aca008504a817c80082520894b2bb2b958afa2e96dab3f3ce7162b87daea39017872386f26fc1000080c001a0884850dc596eac6b74175d2c62deedd9295570808882b0cd9adf47e5ac8b3b3da068881b0ef002d48ef78374d6842ee4987a222a4726af47b5a0a4bcb8f38e2cf3")
 }
---
+++ src/signer.rs
@@ -0,0 +1,93 @@
+use primitive_types::{H256, U256};
+use rlp::RlpStream;
+use secp256k1::recovery::SecretKey;
+use secp256k1::{Message, Secp256k1};
+use std::str::FromStr;
+use tiny_keccak::{Hasher, Keccak};
+
+pub struct Signature {
+    pub v: u64,
+    pub r: H256,
+    pub s: H256,
+}
+
+pub struct Type2Transaction {
+    pub chain_id: U256,
+    pub nonce: U256,
+    pub max_priority_fee_per_gas: U256,
+    pub max_fee_per_gas: U256,
+    pub gas_limit: U256,
+    pub to: U256,
+    pub value: U256,
+    pub data: U256,
+    pub access_list: U256,
+}
+
+pub struct Signer {
+    private_key: SecretKey,
+}
+
+impl Signer {
+    pub fn new(private_key: String) -> Signer {
+        Signer {
+            private_key: SecretKey::from_str(&private_key).unwrap(),
+        }
+    }
+
+    pub fn sign_type2_tx(&self, tx: Type2Transaction) -> String {
+        let to_sign = serialize_type2_tx(&tx, None);
+        let sig = sign(self.private_key, to_sign.as_ref());
+        return hex::encode(serialize_type2_tx(&tx, Some(sig)));
+    }
+}
+
+fn rlp_append_signature(stream: &mut RlpStream, sig: Signature) {
+    stream.append(&sig.v);
+    stream.append(&U256::from_big_endian(sig.r.as_bytes()));
+    stream.append(&U256::from_big_endian(sig.s.as_bytes()));
+}
+
+fn serialize_type2_tx(tx: &Type2Transaction, sig: Option<Signature>) -> Vec<u8> {
+    let mut stream = RlpStream::new();
+
+    stream.begin_list(if sig.is_some() { 12 } else { 9 });
+
+    stream.append(&tx.chain_id);
+    stream.append(&tx.nonce);
+    stream.append(&tx.max_priority_fee_per_gas);
+    stream.append(&tx.max_fee_per_gas);
+    stream.append(&tx.gas_limit);
+    stream.append(&tx.to);
+    stream.append(&tx.value);
+    stream.append(&tx.data);
+
+    // Dont serialize Access List right now
+    stream.begin_list(0);
+
+    if let Some(sig) = sig {
+        rlp_append_signature(&mut stream, sig);
+    }
+
+    return [&[2 as u8], stream.as_raw()].concat();
+}
+
+fn sign(private_key: SecretKey, msg: &[u8]) -> Signature {
+    let hashed = keccak256(msg);
+    let secp = Secp256k1::new();
+    let msg = Message::from_slice(&hashed).unwrap();
+    let signed = secp.sign_recoverable(&msg, &private_key);
+    let (recovery, sig) = signed.serialize_compact();
+    return Signature {
+        v: recovery.to_i32() as u64,
+        r: H256::from_slice(&sig[..32]),
+        s: H256::from_slice(&sig[32..]),
+    };
+}
+
+fn keccak256(data: &[u8]) -> [u8; 32] {
+    let mut hasher = Keccak::v256();
+    hasher.update(data);
+    let mut output = [0u8; 32];
+    hasher.finalize(&mut output);
+    return output;
+}
